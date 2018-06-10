e4de0d9e8ff96ea5c1f6f46f9f25cca6af492302
Benjamin Liu Po-Ching <benjamin.liu@titansoft.com.sg>
2018-06-10 22:59:16 +0800

    initial project

---
+++ TicTacToeMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TicTacToeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Input()
+        {
+            return View("Index");
+        }
+    }
+}
